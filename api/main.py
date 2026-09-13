2f84d134f51582d575b22752eeb390ebe1da567c
Ahmed Gelwan <89267727+ahmedmgelwan@users.noreply.github.com>
2026-09-13 00:45:08 +0000

    adding /diacritize endpoint

--- api/main.py
+++ api/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
-from src.config import model_info
+from src.config import model_info, PathConfig, DEVICE
+from src.infer import Diacritizer
+from contextlib import asynccontextmanager
+from pydantic import BaseModel
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Initialize the diacritizer
+    diacritizer = Diacritizer.from_pretrained(
+        model_path=PathConfig.model_path,
+        tokenizer_path=PathConfig.tokenizer_path,
+        device=DEVICE
+    )
+    app.state.diacritizer = diacritizer
+    yield
+  
+
+app = FastAPI(title=f'{model_info.model_name} API',
+                         lifespan=lifespan)
 
 @app.get("/health")
 def health():
@@ -11,3 +27,15 @@ def health():
         "model_version": model_info.model_version,
         "model_description": model_info.model_description,
     } 
+
+class DiacritizationRequest(BaseModel):
+    text: str
+
+class DiacritizationResponse(BaseModel):
+    diacritized_text: str
+
+@app.post("/diacritize", response_model=DiacritizationResponse)
+def diacritize(request: DiacritizationRequest):
+    diacritizer: Diacritizer = app.state.diacritizer
+    diacritized_text = diacritizer.diacritize(request.text)
+    return DiacritizationResponse(diacritized_text=diacritized_text)
